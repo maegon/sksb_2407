3c72ece15671cfd6402b6bff750d8934b4645896
maegon <dlagorhs99@naver.com>
2024-07-01 18:51:38 +0900

    member domain

---
+++ src/main/java/com/example/sksb/domain/member/entity/Member.java
@@ -0,0 +1,40 @@
+package com.example.sksb.domain.member.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Member {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+    
+    @CreatedDate
+    private LocalDateTime createDate;
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
+    private String username;
+    private String password;
+    
+    // 사용자의 권한을 설정할 때 사용 (롤 멤버면 기본 권한을 부여)
+    public List<String> getAuthoritiesAsStringList() {
+        return List.of("ROLE_MEMBER");
+    }
+    
+}
