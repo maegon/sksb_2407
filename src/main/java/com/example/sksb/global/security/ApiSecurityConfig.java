7e4cfcbfc6a6ebc8bc15cff3bb7a526ce1f3221d
maegon <dlagorhs99@naver.com>
2024-07-02 16:49:49 +0900

    set context auth

--- src/main/java/com/example/sksb/global/security/ApiSecurityConfig.java
+++ src/main/java/com/example/sksb/global/security/ApiSecurityConfig.java
@@ -1,12 +1,18 @@
 package com.example.sksb.global.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity  // 추가적인 보안이 이뤄지기 위한 어노테이션
+@RequiredArgsConstructor
 public class ApiSecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
     @Bean
     SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
         http
@@ -15,8 +21,14 @@ public class ApiSecurityConfig {
                         csrf -> csrf
                                 .disable()
                 )
+                .sessionManagement(
+                        sessionManagement -> sessionManagement
+                                .disable()
+                )
                 .cors(cors -> cors
                         .configure(http)
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class
                 );
         return http.build();
     }
