7057f48a9bf1bdcba2ccdf2b3b6a56e5d3ff4bdb
maegon <dlagorhs99@naver.com>
2024-07-02 16:14:49 +0900

    add MemberDto

--- src/main/java/com/example/sksb/domain/member/controller/ApiV1MembersController.java
+++ src/main/java/com/example/sksb/domain/member/controller/ApiV1MembersController.java
@@ -1,5 +1,6 @@
 package com.example.sksb.domain.member.controller;
 
+import com.example.sksb.domain.member.dto.MemberDto;
 import com.example.sksb.domain.member.service.MemberService;
 import com.example.sksb.global.rq.Rq;
 import com.example.sksb.global.rsData.RsData;
@@ -24,10 +25,7 @@ public class ApiV1MembersController {
     @AllArgsConstructor
     @Getter
     public static class LoginResponseBody {
-        @NotBlank
-        private String refreshToken;
-        @NotBlank
-        private String accessToken;
+        private MemberDto item;
     }
 
     @Getter
@@ -52,8 +50,9 @@ public class ApiV1MembersController {
                 authAndMakeTokensRs.getResultCode(),
                 authAndMakeTokensRs.getMsg(),
                 new LoginResponseBody(
-                        authAndMakeTokensRs.getData().getRefreshToken(),
-                        authAndMakeTokensRs.getData().getAccessToken()
+                        new MemberDto(
+                                authAndMakeTokensRs.getData().getMember()
+                        )
                 )
         );
     }
